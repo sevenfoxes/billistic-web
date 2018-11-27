985371b0e551dce1cb28c59a1faeb968bf874904
Scott Nesham <sanesham@gmail.com>
2018-11-27 00:37:45 -0600

    make the expense list look a bit nicer

--- src/features/Expenses/ExpenseList.js
+++ src/features/Expenses/ExpenseList.js
@@ -3,13 +3,14 @@ import "./expense_list.scss"
 import { compose } from "recompose"
 import { withExpenseHandlers, withExpenseData } from './Expense.hocs'
 
+import Checkbox from '@material-ui/core/Checkbox'
+import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import Checkbox from '@material-ui/core/Checkbox'
+import Typography from '@material-ui/core/Typography'
 
 const Expenses = ({ expenses, togglePaid }) => expenses.map(expense => (
   <TableRow key={expense.id}>
@@ -23,7 +24,10 @@ const Expenses = ({ expenses, togglePaid }) => expenses.map(expense => (
 ))
 
 export const billList = ({ expenses, togglePaid }) => (
-  <Paper>
+  <Paper className="expense_list">
+    <Typography className="expense_list-title" variant="h5" component="h2">
+      Expenses
+    </Typography>
     <Table>
       <TableHead>
         <TableRow>
